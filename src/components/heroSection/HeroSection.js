e7cc57b87ce4971fc376280ad0f7623ce93bc91b
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-07-26 11:31:52 -0700

    completed herosection component

---
+++ src/components/heroSection/HeroSection.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './HeroSection.css'
+import Button from '../utilities/Button'
+import kendo from '../../assets/kendo.png'
+
+export default function HeroSection() {
+  return (
+    <section className="hero-section">
+        <div className="text-box">
+            <p>Hi, My Name is </p>
+            <h1>Kennedy Emeruem.</h1>
+            <h2>Full Stack Web Developer</h2>
+            <p> I'm a full stack web developer with a passion for creating clean and elegant web applications.</p>
+            <Button cName='btn btn-primary' children='Contact Me' />
+        </div>
+        <div className='hero-image'>
+            <img src={kendo} alt="hero" />
+        </div>
+    </section>
+  )
+}
